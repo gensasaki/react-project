da88ef9f823789688c8edf9c739165ca3755ac6e
Gen Sasaki <sasakigen11@gmail.com>
2017-10-16 22:28:31 -0700

    Fetch geolocate api

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
 import SearchForm from './SearchForm';
-import GeocodeResult from './GeocodeResult'
+import GeocodeResult from './GeocodeResult';
+
+const GEOCODE_ENDPOINT = 'https://maps.googleapis.com/maps/api/geocode/json';
 
 class App extends Component {
   constructor(props) {
@@ -9,15 +13,49 @@ class App extends Component {
     };
   }
 
+  setErrorMessage(errorMessage) {
+    this.setState({
+      address: errorMessage,
+      lat: 0,
+      lng: 0,
+    });
+  }
+
   handlePlaceSubmit(place) {
-    console.log(place);
+    axios
+      .get(GEOCODE_ENDPOINT, { params: { address: place } })
+      .then((results) => {
+        const { data } = results.data;
+        const result = data.results[0];
+        switch (data.status) {
+          case 'OK': {
+            const { location } = result.geometry.location;
+            this.setState({
+              address: result.formatted_address,
+              lat: location.lat,
+              lng: location.lng,
+            });
+            break;
+          }
+          case 'ZERO_RESULTS': {
+            this.setErrorMessage('Not found.');
+            break;
+          }
+          default: {
+            this.setErrorMessage('An error occured.');
+          }
+        }
+      })
+      .catch(() => {
+        this.setErrorMessage('Failed to connect.');
+      });
   }
 
   render() {
     return (
       <div>
         <h1>Latitude Longitude Search</h1>
-        <SearchForm onSubmit={place => this.handlePlaceSubmit(place)}/>
+        <SearchForm onSubmit={place => this.handlePlaceSubmit(place)} />
         <GeocodeResult
           address={this.state.address}
           lat={this.state.lat}

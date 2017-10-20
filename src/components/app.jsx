811e2ac998352b281fcbdbdfe8d453b471221fbf
Gen Sasaki <sasakigen11@gmail.com>
2017-10-20 00:44:52 -0700

    Divide seach page and add navbar

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,94 +1,15 @@
-import React, { Component } from 'react';
-import _ from 'lodash';
+import React from 'react';
 
-import SearchForm from './SearchForm';
-import GeocodeResult from './GeocodeResult';
-import Map from './Map';
-import HotelsTable from './HotelsTable';
+import SearchPage from './SearchPage';
 
-import { geocode } from '../domain/Geocoder';
-import { searchHotelByLocation } from '../domain/HotelRepository';
-
-const sortedHotels = (hotels, sortKey) => _.sortBy(hotels, h => h[sortKey]);
-
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      location: {
-        lat: 35.6489643,
-        lng: 139.7429376,
-      },
-      sortKey: 'price',
-    };
-  }
-
-  setErrorMessage(errorMessage) {
-    this.setState({
-      address: errorMessage,
-      location: {
-        lat: 0,
-        lng: 0,
-      },
-    });
-  }
-
-  handlePlaceSubmit(place) {
-    geocode(place)
-      .then(({ status, address, location }) => {
-        switch (status) {
-          case 'OK': {
-            this.setState({ address, location });
-            return searchHotelByLocation(location);
-          }
-          case 'ZERO_RESULTS': {
-            this.setErrorMessage('Not found.');
-            break;
-          }
-          default: {
-            this.setErrorMessage('An error occured.');
-          }
-        }
-        return [];
-      })
-      .then((hotels) => {
-        this.setState({ hotels: sortedHotels(hotels, this.state.sortKey) });
-      })
-      .catch(() => {
-        this.setErrorMessage('Failed to connect.');
-      });
-  }
-
-  handleSortKeyChange(sortKey) {
-    this.setState({
-      sortKey,
-      hotels: sortedHotels(this.state.hotels, sortKey),
-    });
-  }
-
-  render() {
-    return (
-      <div className="app">
-        <h1>Search Hotel in Japan</h1>
-        <SearchForm onSubmit={place => this.handlePlaceSubmit(place)} />
-        <div className="result-area">
-          <Map location={this.state.location} />
-          <div className="result-right">
-            <GeocodeResult
-              address={this.state.address}
-              location={this.state.location}
-            />
-            <h2>Hotel Search Results</h2>
-            <HotelsTable
-              hotels={this.state.hotels}
-              sortKey={this.state.sortKey}
-              onSort={sortKey => this.handleSortKeyChange(sortKey)}
-            />
-          </div>
-        </div>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="app">
+    <ul className="left-navi">
+      <li><a href="/">Search Hotel</a></li>
+      <li><a href="/about">About</a></li>
+    </ul>
+    <SearchPage />
+  </div>
+);
 
 export default App;

89c1cb10fb4046e82078f109f5106df9b80c50e1
Gen Sasaki <sasakigen11@gmail.com>
2017-10-21 14:14:55 -0700

    Move SearchPage.jsx into containers

--- src/containers/SearchPage.jsx
+++ src/containers/SearchPage.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import _ from 'lodash';
 import queryString from 'query-string';
 
-import SearchForm from './SearchForm';
+import SearchForm from '../components/SearchForm';
 // import GeocodeResult from './GeocodeResult';
 // import Map from './Map';
 // import HotelsTable from './HotelsTable';
@@ -27,10 +27,17 @@ class SearchPage extends Component {
   }
 
   componentDidMount() {
-    const place = this.getPlaceParam();
-    if (place) {
-      this.startSearch(place);
-    }
+    this.unsubscribe = this.props.store.subscribe(() => {
+      this.forceUpdate();
+    });
+    // const place = this.getPlaceParam();
+    // if (place) {
+    //   this.startSearch(place);
+    // }
+  }
+
+  componentWillUnmount() {
+    this.unsubscribe();
   }
 
   getPlaceParam() {
@@ -54,7 +61,10 @@ class SearchPage extends Component {
 
   handlePlaceChange(e) {
     e.preventDefault();
-    this.props.onPlaceChange(e.target.value);
+    this.props.store.dispatch({
+      type: 'CHANGE_PLACE',
+      place: e.target.value,
+    });
   }
 
   handlePlaceSubmit(e) {
@@ -97,11 +107,12 @@ class SearchPage extends Component {
   }
 
   render() {
+    const state = this.props.store.getState();
     return (
       <div className="search-page">
         <h1 className="app-title">Search Hotel in Japan</h1>
         <SearchForm
-          place={this.props.place}
+          place={state.place}
           onPlaceChange={e => this.handlePlaceChange(e)}
           onSubmit={e => this.handlePlaceSubmit(e)}
         />
@@ -128,8 +139,11 @@ class SearchPage extends Component {
 SearchPage.propTypes = {
   history: PropTypes.shape({ push: PropTypes.func }).isRequired,
   location: PropTypes.shape({ search: PropTypes.string }).isRequired,
-  onPlaceChange: PropTypes.func.isRequired,
-  place: PropTypes.string.isRequired,
+  store: PropTypes.shape({
+    subscribe: PropTypes.func,
+    getState: PropTypes.func,
+    dispatch: PropTypes.func,
+  }).isRequired,
 };
 
 export default SearchPage;

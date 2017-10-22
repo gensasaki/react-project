977d6eac74146d9b98c2c575f99d229646fa7da1
Gen Sasaki <sasakigen11@gmail.com>
2017-10-21 18:25:14 -0700

    Add redux devtools

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,5 +13,9 @@ module.exports = {
         "event",
         "fdescribe",
       ],
+      "no-underscore-dangle": [
+        "error",
+        { "allow": ["__REDUX_DEVTOOLS_EXTENSION__"] },
+      ]
     }
 };

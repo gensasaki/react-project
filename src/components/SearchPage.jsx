0fc01c015475ec0924d04a92ed5a4ae336494a5f
Gen Sasaki <sasakigen11@gmail.com>
2017-10-21 21:41:29 -0700

    Divide Reducer

--- src/components/SearchPage.jsx
+++ src/components/SearchPage.jsx
@@ -1,15 +1,13 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import _ from 'lodash';
 import queryString from 'query-string';
 
 import SearchForm from '../containers/SearchForm';
-// import GeocodeResult from './GeocodeResult';
-// import Map from './Map';
+import GeocodeResult from './GeocodeResult';
+import Map from './Map';
 // import HotelsTable from './HotelsTable';
-
-import { geocode } from '../domain/Geocoder';
-import { searchHotelByLocation } from '../domain/HotelRepository';
 
 const sortedHotels = (hotels, sortKey) => _.sortBy(hotels, h => h[sortKey]);
 
@@ -17,11 +15,6 @@ class SearchPage extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      place: this.getPlaceParam() || 'Keio University',
-      location: {
-        lat: 35.6489643,
-        lng: 139.7429376,
-      },
       sortKey: 'price',
     };
   }
@@ -52,37 +45,11 @@ class SearchPage extends Component {
     });
   }
 
-  handlePlaceSubmit(e) {
-    e.preventDefault();
-    this.props.history.push(`/?place=${this.state.place}`);
-    this.startSearch();
-  }
-
-  startSearch() {
-    geocode(this.state.place)
-      .then(({ status, address, location }) => {
-        switch (status) {
-          case 'OK': {
-            this.setState({ address, location });
-            return searchHotelByLocation(location);
-          }
-          case 'ZERO_RESULTS': {
-            this.setErrorMessage('Not found.');
-            break;
-          }
-          default: {
-            this.setErrorMessage('An error occured.');
-          }
-        }
-        return [];
-      })
-      .then((hotels) => {
-        this.setState({ hotels: sortedHotels(hotels, this.state.sortKey) });
-      })
-      .catch(() => {
-        this.setErrorMessage('Failed to connect.');
-      });
-  }
+  // handlePlaceSubmit(e) {
+  //   e.preventDefault();
+  //   this.props.history.push(`/?place=${this.state.place}`);
+  //   this.startSearch();
+  // }
 
   handleSortKeyChange(sortKey) {
     this.setState({
@@ -95,32 +62,43 @@ class SearchPage extends Component {
     return (
       <div className="search-page">
         <h1 className="app-title">Search Hotel in Japan</h1>
-        <SearchForm
-          onSubmit={e => this.handlePlaceSubmit(e)}
-        />
-        {/* <div className="result-area">
-          <Map location={this.state.location} />
+        <SearchForm />
+        <div className="result-area">
+          <Map location={this.props.geocodeResult.location} />
           <div className="result-right">
             <GeocodeResult
-              address={this.state.address}
-              location={this.state.location}
+              address={this.props.geocodeResult.address}
+              location={this.props.geocodeResult.location}
             />
+            {/*
             <h2>Hotel Search Results</h2>
             <HotelsTable
               hotels={this.state.hotels}
               sortKey={this.state.sortKey}
               onSort={sortKey => this.handleSortKeyChange(sortKey)}
             />
+            */}
           </div>
-        </div> */}
+        </div>
       </div>
     );
   }
 }
 
 SearchPage.propTypes = {
-  history: PropTypes.shape({ push: PropTypes.func }).isRequired,
+  // history: PropTypes.shape({ push: PropTypes.func }).isRequired,
   location: PropTypes.shape({ search: PropTypes.string }).isRequired,
+  geocodeResult: PropTypes.shape({
+    address: PropTypes.string.isRequired,
+    location: PropTypes.shape({
+      lat: PropTypes.number.isRequired,
+      lng: PropTypes.number.isRequired,
+    }),
+  }),
 };
 
-export default SearchPage;
+const mapStateToProps = state => ({
+  geocodeResult: state.geocodeResult,
+});
+
+export default connect(mapStateToProps)(SearchPage);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,33 @@
-export default (state = { place: 'hoge' }, action) => {
+import { combineReducers } from 'redux';
+
+const place = (state = 'Tokyo Tower', action) => {
   switch (action.type) {
     case 'CHANGE_PLACE':
-      return Object.assign({}, state, { place: action.place });
+      return action.place;
     default:
       return state;
   }
 };
+
+const geocodeResult = (
+  state = {
+    address: '',
+    location: {
+      lat: 35.6489643,
+      lng: 139.7429376,
+    },
+  },
+  action,
+) => {
+  switch (action.type) {
+    case 'GEOCODE_FETCHED':
+      return {
+        address: action.address,
+        location: action.location,
+      };
+    default:
+      return state;
+  }
+};
+
+export default combineReducers({ place, geocodeResult });

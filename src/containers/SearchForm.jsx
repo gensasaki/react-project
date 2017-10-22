98672b43f64b3230fb41805c18db0b344a65b1df
Gen Sasaki <sasakigen11@gmail.com>
2017-10-21 23:34:56 -0700

    Divide action creater

--- src/containers/SearchForm.jsx
+++ src/containers/SearchForm.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-
-import { geocode } from '../domain/Geocoder';
 // import { searchHotelByLocation } from '../domain/HotelRepository';
+import { setPlace, startSearch } from '../actions/';
 
 const SearchForm = props => (
   <form className="search-form"
     onSubmit={e => {
       e.preventDefault();
-      props.onSubmit(props.place)
+      props.startSearch();
     }}
   >
     <input
@@ -19,7 +18,7 @@ const SearchForm = props => (
       value={props.place}
       onChange={(e) => {
         e.preventDefault();
-        props.onPlaceChange(e.target.value);
+        props.setPlace(e.target.value);
       }}
     />
     <input className="submit-button" type="submit" value="Search" />
@@ -28,44 +27,13 @@ const SearchForm = props => (
 
 SearchForm.propTypes = {
   place: PropTypes.string.isRequired,
-  onPlaceChange: PropTypes.func.isRequired,
-  onSubmit: PropTypes.func.isRequired,
+  setPlace: PropTypes.func.isRequired,
+  startSearch: PropTypes.func.isRequired,
 };
 
-const mapStateToProps = state => ({
-  place: state.place,
-});
-const mapDispatchToProps = dispatch => ({
-  onPlaceChange: place => dispatch({
-    type: 'CHANGE_PLACE',
-    place,
+export default connect(
+  state => ({
+    place: state.place,
   }),
-  onSubmit: (place) => {
-    geocode(place)
-      .then(({ status, address, location }) => {
-        switch (status) {
-          case 'OK': {
-            dispatch({ type: 'GEOCODE_FETCHED', address, location });
-            // return searchHotelByLocation(location);
-            break;
-          }
-          case 'ZERO_RESULTS': {
-            // this.setErrorMessage('Not found.');
-            break;
-          }
-          default: {
-            // this.setErrorMessage('An error occured.');
-          }
-        }
-        return [];
-      });
-      // .then((hotels) => {
-      //   this.setState({ hotels: sortedHotels(hotels, this.state.sortKey) });
-      // })
-      // .catch(() => {
-      //   this.setErrorMessage('Failed to connect.');
-      // });
-  },
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(SearchForm);
+  { setPlace, startSearch },
+)(SearchForm);
